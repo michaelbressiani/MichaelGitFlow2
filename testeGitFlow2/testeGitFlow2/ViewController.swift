c96dd3b11c7c08702734808fd7b72bfe5343962f
Michael Bressiani <>
2023-08-22 22:32:07 -0300

    Criando projeto

---
+++ testeGitFlow2/testeGitFlow2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testeGitFlow2
+//
+//  Created by Michael Bressiani on 22/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testeGitFlow2/testeGitFlow2UITests/testeGitFlow2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  testeGitFlow2UITestsLaunchTests.swift
+//  testeGitFlow2UITests
+//
+//  Created by Michael Bressiani on 22/08/23.
+//
+
+import XCTest
+
+final class testeGitFlow2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
